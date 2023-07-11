a7bfffce3b2a2a6826127c394871a01b676c29ca
Henry Ramírez <luishenryramirezmontalvo746@gmail.com>
2023-07-11 09:17:04 -0500

    Validacion Factory Detalles

--- database/factories/DetalleFacturasFactory.php
+++ database/factories/DetalleFacturasFactory.php
@@ -18,7 +18,7 @@ class DetalleFacturasFactory extends Factory
     {
         return [
             'factura_id' => $this->faker->randomElement([1, 2, 3]),
-            'producto_id' => $this->faker->numberBetween(1, 100),
+            'producto_id' => $this->faker->randomElement([1, 2, 3, 4, 5, 6, 7, 8, 9]),
             'cantidad' => $this->faker->numberBetween(1, 10),
             'subtotal' => $this->faker->randomFloat(2, 10, 100),
             'total' => $this->faker->randomFloat(2, 10, 100),
